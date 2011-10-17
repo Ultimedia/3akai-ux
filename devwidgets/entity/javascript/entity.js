2a08a5f32385f04d1248029f2bd1b78669139455
Maarten Bressinck <maartenbressinck@gmail.com>
2011-10-17 10:36:25 +0100

    Templategenerator, the first version of th etemplategenerator Widget

--- devwidgets/entity/javascript/entity.js
+++ devwidgets/entity/javascript/entity.js
@@ -340,6 +340,13 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
                 $('#entity_contentsettings_dropdown').jqmHide();
             });
 
+			// templateGenerator
+            $('#ew_group_export_as_template_link').click(function(e){
+				e.preventDefault();
+				$(window).trigger("init.templategenerator.sakai");
+				$('#entity_groupsettings_dropdown').jqmHide();
+            });
+
             $('#ew_upload').click(function(e){
                 e.preventDefault();
                 $(window).trigger("init.uploadnewversion.sakai");
