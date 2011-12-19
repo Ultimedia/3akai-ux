24f3fe0e68eea973f0c7a2d5b9790a46b4b0353e
msn <daniel@caret.cam.ac.uk>
2011-12-19 06:43:14 +0000

    Reconciling some minor differences from the 1.1 tag.

--- devwidgets/entity/javascript/entity.js
+++ devwidgets/entity/javascript/entity.js
@@ -438,13 +438,6 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
                 $('#entity_groupsettings_dropdown').jqmHide();
             });
 
-			// templateGenerator
-            $('#ew_group_export_as_template_link').click(function(e){
-				e.preventDefault();
-				$(window).trigger("init.templategenerator.sakai");
-				$('#entity_groupsettings_dropdown').jqmHide();
-            });
-
             $('#ew_upload').click(function(e){
                 e.preventDefault();
                 $(window).trigger("init.uploadnewversion.sakai");
